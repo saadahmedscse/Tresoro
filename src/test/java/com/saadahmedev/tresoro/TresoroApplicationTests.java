2cb266cb12be00fe8d4e764ef407c9f8a16975cc
saadahmedscse <saadahmedscse@gmail.com>
2023-12-18 21:37:49 +0600

    initial commit

---
+++ src/test/java/com/saadahmedev/tresoro/TresoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadahmedev.tresoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresoroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
